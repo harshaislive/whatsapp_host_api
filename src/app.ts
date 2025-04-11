07011919aa358954a76ad02cdfe7148fcb3a7399
harshaislive <harsha.mudumba@gmail.com>
2025-04-11 12:35:01 +0530

    feat: add CORS middleware to enable Swagger UI requests

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 import whatsappService from './services/whatsapp.service';
 import messageRoutes from './routes/message.routes';
 import { swaggerDocument } from './config/swagger';
@@ -13,6 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
